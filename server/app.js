a29c6f0048f8f296e1f97c42f099fb7707fb98c1
loolys <pierre.liljekvist@outlook.com>
2017-02-09 07:52:21 +0100

    Created a database that saves new user with encrypted password

--- server/app.js
+++ server/app.js
@@ -3,8 +3,18 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const users = require('./routes/users');
+const UserModel = require('./models/user-model');
+
+const connStr = 'mongodb://localhost:27017/mongoose-bcrypt-test';
+mongoose.connect(connStr);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connect to db');
+});
 
 const app = express();
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const validateInput = require('../shared/validations/signup');
+const UserModel = require('../models/user-model');
 
 let router = express.Router();
 
@@ -7,7 +8,18 @@ router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body);
 
   if (isValid) {
-    res.json({ success: true });
+    const { username, password } = req.body;
+
+    const user = new UserModel({
+      username: username,
+      password: password
+    });
+
+    user.save(function (err) {
+      if (err) res.status(500).json({ error: err });
+      console.log('saved user');
+    }).then( res.json({ success: true }));
+
   } else {
     res.status(400).json(errors);
   }
